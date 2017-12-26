aa750c8f56213f867bf51197d66c637b8fa86adc
SaeedRafay <saeedahmadrafay@hotmail.com>
2017-12-26 18:03:33 +0500

    Rendering a button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+class Button extends React.Component {
+	state = { counter: 0 };
+
+	render() {
+		return (
+			<button>
+				{ this.state.counter }
+			</button>
+		);
+	}
+}
+
+ReactDOM.render(<Button />, document.getElementById('root'));
