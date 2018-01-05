911aa839cfc5872a76e4dc3f78308a637453c3c1
SAEED-RAFAY\Saeed.Rafay <saeedahmadrafay@hotmail.com>
2018-01-05 13:11:09 +0500

    no message

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-class Button extends React.Component {
-	state = { counter: 0 };
-
-	render() {
-		return (
-			<button>
-				{ this.state.counter }
-			</button>
-		);
-	}
-}
-
-ReactDOM.render(<Button />, document.getElementById('root'));
