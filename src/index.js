9e8faa34a13b501ae1c79276d979cb9ec0383c91
SAEED-RAFAY\Saeed.Rafay <saeedahmadrafay@hotmail.com>
2018-01-08 02:38:02 +0500

    Listing Hotels with Sorting by Name and Price

--- src/index.js
+++ src/index.js
@@ -1,29 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Container, Row, Col } from 'reactstrap';
+import { Button, Card, CardBody } from 'reactstrap';
 import './index.css';
 
 const HotelCard = (props) => {
   return (
-    <div className="card">
-      <div className="card-header">
-        <strong>Name: </strong> { props.name }
-      </div>
-      <div className="card-body">
-        <strong>Price: </strong> { props.price }<br/>
-        <strong>City: </strong> { props.city }<br/>
-        <strong>Availability: </strong><br/>
-        { JSON.stringify(props.availability) }
-      </div>
+    <Col md="6">
+      <Card>
+        <CardBody>
+          <strong>Name: </strong> { props.name }<br/>
+          <strong>Price: </strong> { props.price }<br/>
+          <strong>City: </strong> { props.city }<br/>
+        </CardBody>
+      </Card>
+    </Col>
+  )
+}
+
+const HotelRow = (props) => {
+  return (
+    <Row>
+      {
+        props.hotelPair.map((hotel, index) => (
+          <HotelCard key={index} {...hotel} />
+        ))
+      }
+    </Row>
+  )
+}
+
+const Hotels = (props) => {
+  return (
+    <div className="hotels_list">
+      {
+        props.rpHotels.reduce((pairs, hotel, index) => {
+          if(index % 2 === 0) {
+             pairs.push([]);
+          }
+          pairs[pairs.length - 1].push(hotel);
+          return pairs;
+        }, []).map((pair, index) => (
+          <HotelRow key={index} hotelPair={pair} />
+        ))
+      }
     </div>
   )
 }
 
-class Hotels extends React.Component {
+const HotelSort = (props) => {
+  return (
+    <div className="hotels_sort text-right">
+      <Button color="primary" onClick={() => props.onSort('name')}>Sort By Name</Button>{' '}
+      <Button color="primary" onClick={() => props.onSort('price')}>Sort By Price</Button>
+    </div>
+  )
+}
 
+class App extends React.Component {
   state = {
     rHotels: [],
-    counter: 1,
   }
 
   componentDidMount() {
@@ -34,15 +71,23 @@ class Hotels extends React.Component {
       });
   }
 
+  sortByKey = (key) => {
+    this.setState((prevState) => ({
+      rHotels: prevState.rHotels.sort((a, b) => {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+      }),
+    }))
+  };
+
   render() {
     return (
-      <div>
-        {this.state.rHotels.map((hotel) =>
-          <HotelCard key={this.state.counter++} {...hotel}  />
-        )}
-      </div>
-    );
+      <Container>
+        <HotelSort onSort={this.sortByKey} />
+        <Hotels rpHotels={this.state.rHotels} />
+      </Container>
+    )
   }
 }
 
-ReactDOM.render(<Hotels />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
